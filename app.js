efc3d82866750afc0c654de7ddbb2bbc05daeb5f
amirhsgh <amiqavi2601@gmail.com>
2023-01-12 15:25:47 +0330

    feat: add python script

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const {getQuestions, sendQuestions} = require('./controllers')
 const port = 3000
 
 const fileUpload = require('express-fileupload');
@@ -8,9 +8,16 @@ const fileUpload = require('express-fileupload');
 app.use(express.json());
 app.use(fileUpload());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", async (req, res) => {
+  res.send({"message":"Hello"})
 })
+
+app.post("/api/v1/get_questions", async (req, res) => {
+  await getQuestions(req, res);
+});
+app.get("/api/v1/send_questions", async (req, res) => {
+  await sendQuestions(req, res);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
