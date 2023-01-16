d5f20299794d3aa1f7f1cf9650c8a277318300ba
amirhsgh <amiqavi2601@gmail.com>
2023-01-16 14:18:02 +0330

    feat:add query to getquestion

--- controllers/getQuestions.js
+++ controllers/getQuestions.js
@@ -6,23 +6,28 @@ const {runScript} = require('../utils/runScript')
 
 async function getQuestions(req, res) {
     try {
+        const title = req.body.title;
         const questions = req.body.questions;
         const wallet_address = req.body.wallet_address;
-        query = `relation == 'has' & entity_2 == 'wallet_id_${wallet_address}'`
-        result = JSON.parse(await runScript(query))
-        console.log(result.entity_2);
-        const jsonArray = await csv().fromFile(
-            "./uploaded/toplevelkg/datalattekg.csv"
-        );
-        for (let sen of jsonArray) {
-            console.log(sen.sentence.toString());
+        query_get_user = `relation == 'has' & entity_2 == 'wallet_id_${wallet_address}'`
+        get_owner_kg_user = JSON.parse(await runScript(query_get_user, "./pythonapp/datalattekg.csv"))
+        //TODO check datalattekg if wallet does'nt exist
+        query_get_relation = `relation == 'owns' & entity_1 == '${(Object.values(get_owner_kg_user.entity_1)[0])}'`
+        get_relation_owns = JSON.parse(await runScript(query_get_relation, "./pythonapp/datalattekg.csv"));
+        if (Object.values(get_relation_owns.entity_2)[0] == 'survey_owner_KG') {
+            console.log(Object.values(get_relation_owns.entity_2)[0]);
+        }else {
+            fs.appendFile("./pythonapp/uploaded/toplevelkg/datalattekg.csv", 'data to append', function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+              });
         }
-        const sentence = "";
+
         converter.json2csv(questions, (err, csv) => {
             if (err) {
                 throw err;
             }
-            fs.writeFileSync("./uploaded/questions/questions.csv", csv);
+            fs.writeFileSync("./pythonapp/uploaded/questions/questions.csv", csv);
         });
 
         res.send({
